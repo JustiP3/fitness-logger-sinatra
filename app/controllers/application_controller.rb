92db4ad0ef3b756321419f3fb794077b51ded78e
Justin Phillips <justinphil02@yahoo.com>
2020-02-09 01:56:33 +0000

    build out file structure, add files.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  get '/login' do
+
+  end
+
+  get '/signup' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  post '/signup' do
+
   end
 
 end
